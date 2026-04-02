5ed0972b3c2ac5aea55fcf7d692f16f310bbf7b3
Christoph Bartschat <christoph@8thwall.org>
2026-04-02 15:10:51 -0700

    Add inlinerjs

--- apps/desktop/tools/inliner.sh
+++ apps/desktop/tools/inliner.sh
@@ -5,7 +5,7 @@ cd $(bazel info workspace 2>/dev/null)
 
 build_files=$(find apps/desktop -path apps/desktop/node_modules -prune -o -name "BUILD" -print)
 
-echo "$build_files" | xargs bazel run --run_under="cd $PWD && " //apps/client/inlinerjs
+echo "$build_files" | xargs bazel run --run_under="cd $PWD && " //bzl/inlinerjs
 
 if type buildifier &>/dev/null; then
   echo "$build_files" | xargs buildifier -lint=fix
