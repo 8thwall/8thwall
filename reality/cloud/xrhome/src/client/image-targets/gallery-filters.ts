94215a4345ecf1a1c5e9bbebfe03815fe7d6a880
Christoph Bartschat <christoph@8thwall.org>
2026-04-15 20:41:54 -0700

    [xrhome] Update gallery filter for local targets

---
+++ reality/cloud/xrhome/src/client/image-targets/gallery-filters.ts
@@ -0,0 +1,28 @@
+import type {IImageTarget} from '../common/types/models'
+import type {ImageTargetFilterOptions} from './types'
+
+const targetMatchesFilter = (target: IImageTarget, filter: ImageTargetFilterOptions): boolean => {
+  if (filter.nameLike) {
+    if (!target.name.includes(filter.nameLike)) {
+      return false
+    }
+  }
+
+  if (filter.type && filter.type.length) {
+    if (!filter.type.includes(target.type)) {
+      return false
+    }
+  }
+
+  if (filter.hasMetadata) {
+    if (!target.userMetadata) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export {
+  targetMatchesFilter,
+}
