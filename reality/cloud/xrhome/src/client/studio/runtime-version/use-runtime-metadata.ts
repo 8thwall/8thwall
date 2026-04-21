abaaa7ec0535f653a58b1af795fcda957a56cab4
Christoph Bartschat <christoph@8thwall.org>
2026-04-21 15:33:23 -0700

    [xrhome] Bust metadata cache after completing setup

--- reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
+++ reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
@@ -2,11 +2,13 @@ import {useSuspenseQuery} from '@tanstack/react-query'
 
 import useCurrentApp from '../../common/use-current-app'
 import {getRuntimeMetadata} from '../local-sync-api'
+import {useLocalSyncContext} from '../local-sync-context'
 
 const useRuntimeMetadata = () => {
   const {appKey} = useCurrentApp()
+  const local = useLocalSyncContext()
   return useSuspenseQuery({
-    queryKey: ['runtimeMetadata'],
+    queryKey: ['runtimeMetadata', local.localBuildUrl],
     queryFn: () => getRuntimeMetadata(appKey),
   }).data
 }
