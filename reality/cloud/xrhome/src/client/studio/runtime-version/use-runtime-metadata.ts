c8842dc2d325321630d4365ea1d6dfc4866a9dc8
Christoph Bartschat <christoph@8thwall.org>
2026-05-06 19:28:16 -0700

    [xrhome] Switch to runtime metadata to enable simulator

--- reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
+++ reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
@@ -2,17 +2,31 @@ import {useSuspenseQuery} from '@tanstack/react-query'
 
 import useCurrentApp from '../../common/use-current-app'
 import {getRuntimeMetadata} from '../local-sync-api'
-import {useMaybeLocalSyncContext} from '../local-sync-context'
+
+/* eslint-disable no-await-in-loop */
+
+const getRuntimeMetadataQuery = (appKey: string) => ({
+  queryKey: ['runtimeMetadata', appKey],
+  queryFn: async () => {
+    // await new Promise(r => setTimeout(r, 2500))
+    let iterations = 0
+    while (iterations++ < 10) {
+      try {
+        return await getRuntimeMetadata(appKey)
+      } catch (err) {
+        await new Promise(r => setTimeout(r, 1500))
+      }
+    }
+    throw new Error('Could not load metadata')
+  },
+})
 
 const useRuntimeMetadata = () => {
   const {appKey} = useCurrentApp()
-  const local = useMaybeLocalSyncContext()
-  return useSuspenseQuery({
-    queryKey: ['runtimeMetadata', appKey, local?.localBuildUrl],
-    queryFn: () => getRuntimeMetadata(appKey),
-  }).data
+  return useSuspenseQuery(getRuntimeMetadataQuery(appKey)).data
 }
 
 export {
   useRuntimeMetadata,
+  getRuntimeMetadataQuery,
 }
--- reality/cloud/xrhome/src/client/studio/scene-debug-context.tsx
+++ reality/cloud/xrhome/src/client/studio/scene-debug-context.tsx
@@ -12,7 +12,6 @@ import {useExpanse} from './hooks/expanse'
 import {useSimulator} from '../editor/app-preview/use-simulator-state'
 import {updateObject} from './update-object'
 import {DerivedSceneProvider} from './derived-scene-context'
-import {PlaybackContextProvider} from './playback-context'
 
 type MutateCallback<T> = (old: DeepReadonly<T>) => DeepReadonly<T>
 
@@ -119,9 +118,7 @@ const SceneDebugContext: React.FC<ISceneDebugContext> = ({
   return (
     <SceneContextProvider value={sceneContextValue}>
       <DerivedSceneProvider>
-        <PlaybackContextProvider>
-          {children}
-        </PlaybackContextProvider>
+        {children}
       </DerivedSceneProvider>
     </SceneContextProvider>
   )
