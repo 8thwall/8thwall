65fa9373758a42c3de6b9f943758fc1f0d6aa9e2
Christoph Bartschat <christoph@8thwall.org>
2026-08-07 11:56:26 -0700

    [g8] save inline

--- scripts/inliner.sh
+++ scripts/inliner.sh
@@ -10,11 +10,11 @@ echo "$build_files" | xargs bazel run --run_under="cd $PWD && " //bzl/inlinerjs 
 
 if type buildifier &> /dev/null; then
   if buildifier --version | grep "5.1.0"; then
+    echo "$build_files" | xargs buildifier -lint=fix
+  else 
     echo "Expected buildifier --version to be 5.1.0"
     exit 1
   fi
-
-  echo "$build_files" | xargs buildifier -lint=fix
 else
   echo "buildifier not found, skipped formatting."
 fi
