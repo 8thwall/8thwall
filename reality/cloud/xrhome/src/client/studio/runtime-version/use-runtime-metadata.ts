f6bb5c2487a157339200a2310d19f034d8bd84ba
Christoph Bartschat <christoph@8thwall.com>
2026-08-23 00:24:16 +0000

    [xrhome] Prevent useRuntimeMetadata from causing crashes (#262)

--- reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
+++ reality/cloud/xrhome/src/client/studio/runtime-version/use-runtime-metadata.ts
@@ -5,21 +5,35 @@ import {getRuntimeMetadata} from '../local-sync-api'
 
 /* eslint-disable no-await-in-loop */
 
+const untilAbort = (signal: AbortSignal) => new Promise<void>((resolve) => {
+  if (signal.aborted) {
+    resolve()
+  } else {
+    signal.addEventListener('abort', () => {
+      resolve()
+    })
+  }
+})
+
 const getRuntimeMetadataQuery = (appKey: string) => ({
   queryKey: ['runtimeMetadata', appKey],
-  queryFn: async () => {
+  queryFn: async ({signal}: {signal: AbortSignal}) => {
     // NOTE(christoph): During first time setup, this may mount before the config is fully
     // initialized. After the server starts, it will trigger a refetch regardless, but to avoid
     // error screens on start, absorb failures.
     let iterations = 0
     while (iterations++ < 10) {
+      if (signal.aborted) {
+        throw new Error('runtimeMetadata cancelled')
+      }
       try {
         return await getRuntimeMetadata(appKey)
       } catch (err) {
         await new Promise(r => setTimeout(r, 1500))
       }
     }
-    throw new Error('Could not load metadata')
+    await untilAbort(signal)
+    throw new Error('runtimeMetadata cancelled after timeout')
   },
 })
 
