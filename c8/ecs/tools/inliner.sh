973641d7efd14aef828f7a99d4c05203ad32d1a0
Christoph Bartschat <christoph@8thwall.org>
2026-09-12 01:31:26 -0700

    [bzl] Restore inliner from archive

--- c8/ecs/tools/inliner.sh
+++ c8/ecs/tools/inliner.sh
@@ -9,11 +9,11 @@ echo "$build_files" | xargs bazel run --run_under="cd $PWD && " //bzl/inlinerjs
 
 if [[ "$@" == *"--cc"* ]]; then
   cc_files=$(find c8/ecs -path c8/ecs/node_modules -prune -o -name "*.cc" -print)
-  bazel build //apps/client/inliner
+  bazel build //bzl/inliner
 
   # NOTE(christoph): Inliner trips over itself when run with multiple files at a time.
   for f in $cc_files; do
-    ./bazel-bin/apps/client/inliner/inliner "$f"
+    ./bazel-bin/bzl/inliner/inliner "$f"
   done
 else 
   echo "Skipping C++ files, enable with: npm run inliner -- --cc"
